c08517c6408412b3129bfffddc59509b6b43df49
GerasimosTzivras <45209780+GerasimosTzivras@users.noreply.github.com>
2020-01-21 21:14:28 +0200

    Add files via upload

---
+++ Persistence/Migrations/20191126074625_TestingOne.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class TestingOne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Customer",
+                table: "Activities",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Activities",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telephone",
+                table: "Activities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Customer",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "Telephone",
+                table: "Activities");
+        }
+    }
+}
